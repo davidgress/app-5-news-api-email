5ccd36d689c57d9b1389c3b5da8ec023ebe49000
davidgress <david.e.gress@gmail.com>
2023-08-12 08:01:14 -0400

    Day 27: work with content from api call

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import requests
 
 api_key = "28d7c28066974f13b60bbbf287f3dc49"
 url = "https://newsapi.org/v2/everything?q=tesla&" \
-    "from=2023-07-12&sortBy=publishedAt&apiKey=" \
+    "sortBy=publishedAt&apiKey=" \
     "28d7c28066974f13b60bbbf287f3dc49"
-
+# Make a request
 request = requests.get(url)
-content = request.text
-print(content)
+
+# Get a dictionary with data
+content = request.json()
+
+# Access the article titles and description
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
