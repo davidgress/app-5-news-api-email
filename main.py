e96478aaaf0101bb814ba5e8785b899156039bce
davidgress <david.e.gress@gmail.com>
2023-08-12 08:19:39 -0400

    Day 27: aSend an email with title and descriptions. encode("utf-8") used

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+from send_email import send_email
+
 
 api_key = "28d7c28066974f13b60bbbf287f3dc49"
 url = "https://newsapi.org/v2/everything?q=tesla&" \
@@ -11,6 +13,11 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article['title'] + "\n" + article['description'] + 2*"\n"
+    # print(article["title"])
+    # print(article["description"])
+body = body.encode("utf-8")
+send_email(body)
